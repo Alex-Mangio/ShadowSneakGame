97a4bf968c6c2a11431cdcdd17374d2c9cb7cdb2
Alex-Mangio <alexmangione01@gmail.com>
2025-06-06 07:23:34 +0200

    final

--- sql/sqlBDSito.sql
+++ sql/sqlBDSito.sql
@@ -8,7 +8,17 @@ CREATE TABLE Users(
 	nome varchar(50) NOT NULL,
     cognome varchar(100) NOT NULL,
     username varchar(255) NOT NULL,
-    CONSTRAINT chk_email CHECK (email REGEXP '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$') ENFORCED,
+    email varchar(255) NOT NULL,
     user_password varchar(255) NOT NULL
 );
 
+CREATE TABLE recensioni (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    voto TINYINT NOT NULL CHECK (voto BETWEEN 1 AND 5),
+    descrizione TEXT NULL,
+    data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id_user)
+);
+
+INSERT INTO Users(nome,cognome,username,email,user_password) VALUES("Alex","Mangio","username","ale@gmail.com","password");
